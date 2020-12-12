d9d1ca6af722b100b3be959a28a0402b7b812626
kjbin0420 <kjbin0420@naver.com>
2020-12-12 13:37:58 +0900

    [PATCH] Folder Name

--- 1. Simple restful api with gorilla:mux/go.mod
+++
@@ -1,5 +0,0 @@
-module restful
-
-go 1.15
-
-require github.com/gorilla/mux v1.8.0
--- 1. Simple restful api with gorilla:mux/payload/response/book_response.go
+++
@@ -1,8 +0,0 @@
-package response
-
-type Book struct {
-	ID     string `json:"id"`
-	Name   string `json:"name"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-}
